8b6c74701677cd2c8dd041dbebecda49d3bc5c93
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2017-11-09 13:08:00 +0000

    added condition number threshold to pivoted cholesky

--- raleigh/piv_chol.py
+++ raleigh/piv_chol.py
@@ -4,8 +4,9 @@ import numpy.linalg as nla
 import scipy.linalg as sla
 
 def piv_chol(A, k, eps):
+    lmax = sla.norm(A, ord = 1)
     n = A.shape[0]
-    ind = [i for i in range(n - k)]
+    ind = [i for i in range(n)]
     if k > 0:
         U = sla.cholesky(A[:k,:k])
         A[:k,:k] = U.copy()
@@ -20,7 +21,7 @@ def piv_chol(A, k, eps):
                 #print('swapping %d and %d' % (i, j))
                 A[i, :], A[j, :] = A[j, :], A[i, :].copy()
                 A[:, i], A[:, j] = A[:, j], A[:, i].copy()
-                ind[i - k], ind[j - k] = ind[j - k], ind[i - k]
+                ind[i], ind[j] = ind[j], ind[i]
             A[i, i : n] -= np.dot(A[:i, i].transpose(), A[:i, i : n])
         #print('pivot: %e' % A[i,i])
         if A[i, i] <= eps:
@@ -29,4 +30,22 @@ def piv_chol(A, k, eps):
         A[i, i] = math.sqrt(A[i, i])
         A[i, i + 1 : n] /= A[i, i]
         A[i + 1 : n, i].fill(0.0)
-    return ind, 0, A[n - 1, n - 1]
+        lmin = estimate_lmin(A[: i + 1, : i + 1])
+        print('%e %e %e' % (A[i,i], lmin, lmax))
+        if lmin/lmax <= eps:
+            A[i : n, :].fill(0.0)
+            return ind, n - i, A[i, i]
+    return ind, 0, A[n - 1, n - 1]**2
+
+def estimate_lmin(U):
+    n = U.shape[0]
+    x = np.ones((n,), dtype = np.dtype(U[0,0]))
+    s = np.dot(x, x)
+    for i in range(3):
+        y = sla.solve_triangular(U, x, trans = 2)
+        t = np.dot(y, y)
+        rq = s/t
+        print(i, rq)
+        x = sla.solve_triangular(U, y)
+        s = np.dot(x, x)
+    return rq
--- try/try_piv_chol.py
+++ try/try_piv_chol.py
@@ -10,13 +10,15 @@ def op(x):
     n = x.shape[0]
     y = x.copy()
     for i in range(n):
-        y[i] = (10**i) * x[i]
+        y[i] = (2**i) * x[i]
     return y/sla.norm(y)
 
-n = 7
-x = numpy.ones((n,n))/math.sqrt(n)
+n = 20
+x = numpy.ones((n,n)) #/math.sqrt(n)
 for j in range(n - 1):
     x[:, j + 1] = op(x[:, j])
+for j in range(n):
+    x[:, j] /= sla.norm(x[:, j])
 A = numpy.dot(x.transpose(), x)
 #print(x)
 #print(A)
@@ -29,17 +31,41 @@ A = numpy.dot(x.transpose(), x)
 ##    A[i + 1, i] = -1
 
 U = A.copy()
-ind, failed, last_piv = piv_chol(U, 0, 1e-15)
+ind, failed, last_piv = piv_chol(U, 0, 1e-4)
 #print(U)
 print('pivot order: ', ind)
 print('failed rows: %d' % failed)
-print('last_pivot: %e' % last_piv)
+print('last_pivot squared: %e' % last_piv)
 
 UTU = numpy.dot(U.transpose(), U)
 #print(UTU)
 #print(A[ind,:][:,ind])
 print('factorization error: %e' % sla.norm(UTU - A[ind,:][:,ind]))
 
-U = sla.cholesky(A)
+y = x[:, ind[:n - failed]]
+B = numpy.dot(y.transpose(), y)
+lmd, v = sla.eigh(B)
+print(lmd)
+print('condition number: %e' % (lmd[-1]/lmd[0]))
+U = sla.cholesky(B)
 #print(U)
+#print(numpy.dtype(U[0,0]))
+#estimate_lmin(U)
+#UTU = numpy.dot(U.transpose(), U)
+#lmd, v = sla.eigh(UTU)
+#print(lmd)
+#print('condition number: %e' % (lmd[-1]/lmd[0]))
 
+y, r = sla.qr(y, mode = 'economic')
+z = x - numpy.dot(y, numpy.dot(y.transpose(), x))
+#print(x.shape)
+#print(y.shape)
+#print(z.shape)
+for i in range(n):
+    print(sla.norm(z[:,i])/sla.norm(x[:,i]))
+
+try:
+    U = sla.cholesky(A)
+    #print(U)
+except:
+    print('factorization failed')
