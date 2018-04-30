601780ce9e624c5e3785b45fcce08f8e38651d2c
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2018-04-30 15:20:40 +0100

    initial commit for sparse mkl wrapper

---
+++ sparse_mkl/sparse_mkl.cpp
@@ -0,0 +1,54 @@
+// sparse_mkl.cpp : Defines the exported functions for the DLL application.
+//
+#include "stdafx.h"
+
+#include <mkl.h>
+
+#ifdef _WIN32
+#define EXPORTED_FUNCTION __declspec(dllexport)
+#else
+#define EXPORTED_FUNCTION
+#endif
+
+extern "C"
+EXPORTED_FUNCTION void scsrmm
+(int m, int n, int k, int* ia, int* ja, float* a, float* u, float* v)
+{
+	float alpha = 1.0f;
+	float beta = 0.0f;
+
+	//for (int j = 0; j < k; j++) {
+	//	std::cout << "col " << j << '\n';
+	//	for (int i = 0; i < n; i++) {
+	//		std::cout << u[i + j*n] << '\n';
+	//	}
+	//}
+	int* pb = ia;
+	int* pe = ia + 1;
+	//for (int i = 0; i < m; i++) {
+	//	std::cout << "row " << i << '\n';
+	//	for (int j = pb[i]; j < pe[i]; j++)
+	//		std::cout << ja[j - 1] - 1 << '\t' << a[j - 1] << '\n';
+	//}
+	mkl_scsrmm("N", &m, &k, &n, &alpha, "G", a, ja, pb, pe, u, &n, &beta, v, &m);
+	//for (int j = 0; j < k; j++) {
+	//	std::cout << "col " << j << '\n';
+	//	for (int i = 0; i < m; i++) {
+	//		std::cout << v[i + j*m] << '\n';
+	//	}
+	//}
+}
+
+extern "C"
+EXPORTED_FUNCTION void dcsrmm
+(int m, int n, int k, int* ia, int* ja, double* a, double* u, double* v)
+{
+	double alpha = 1.0;
+	double beta = 0.0;
+
+	int* pb = ia;
+	int* pe = ia + 1;
+	mkl_dcsrmm("N", &m, &k, &n, &alpha, "G", a, ja, pb, pe, u, &n, &beta, v, &m);
+}
+
+
---
+++ sparse_mkl/try_sparse_mkl.py
@@ -0,0 +1,44 @@
+import numpy
+import ctypes
+import scipy.sparse as scsp
+import time
+
+n = 1024*1024
+v = numpy.ones((n,), dtype = numpy.float32)
+
+#dll = ctypes.cdll.LoadLibrary('./x64/Release/try_dll.dll')
+dll = ctypes.CDLL('./sparse_mkl.dll', mode = ctypes.RTLD_GLOBAL)
+
+numpy.random.seed(1) # make results reproducible
+form = 'csr'
+dt = numpy.float32
+
+mA = 40000
+nA = 30000
+nnz = 200
+den = nnz/min(mA, nA)
+print('generating matrix of density %.2e...' % den)
+A = scsp.random(mA, nA, den, form, dtype = dt)
+
+maxk = 16
+k = 4
+
+ntests = 20
+
+x = numpy.random.randn(maxk, nA).astype(dt)
+y = numpy.zeros((maxk, mA), dtype = dt)
+ptr_a = ctypes.c_void_p(A.data.ctypes.data)
+indices = A.indices + 1
+indptr = A.indptr + 1
+ptr_ja  = indices.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
+ptr_ia  = indptr.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
+ptr_u = ctypes.c_void_p(x.ctypes.data)
+ptr_v = ctypes.c_void_p(y.ctypes.data)
+
+start = time.time()
+for t in range(ntests):
+    dll.scsrmm(ctypes.c_int(mA), ctypes.c_int(nA), ctypes.c_int(k), \
+               ptr_ia, ptr_ja, ptr_a, ptr_u, ptr_v)
+stop = time.time()
+time_scsrmm = stop - start
+print('time: %.1e' % (time_scsrmm/k))
