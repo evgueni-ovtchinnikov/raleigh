a5bc7558674e2d728e21f84f78ae9f201fa0f131
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2018-10-02 14:07:45 +0100

    tidied up svds_bug.py

--- try/svds_bug.py
+++ try/svds_bug.py
@@ -5,6 +5,7 @@ Created on Thu Apr  5 11:44:49 2018
 @author: Evgueni Ovtchinnikov, UKRI
 """
 import numpy
+from scipy.linalg import svd
 from scipy.sparse.linalg import svds
 
 def random_singular_values(k, sigma, dt):
@@ -26,24 +27,10 @@ def random_matrix_for_svd(m, n, k, sigma, dt):
     a = numpy.dot(u*s, v.transpose())
     return s, u, v, a
 
-numpy.random.seed(1)
-
-m = 5000
-n = 10000
-l = 400
-alpha = 0.05
-f_sigma = lambda t: 2**(-alpha*t).astype(numpy.float32)
-sigma0, u0, v0, A = random_matrix_for_svd(m, n, l, f_sigma, numpy.float32)
-print('\n 128 largest singular values of A:')
-print(sigma0[::-1])
-u, sigma, vt = svds(A, k = 128)
-print('\n 128 largest singular values from svds:')
-print(sigma)
-
-# another test: this time A is unlikely to have multiple zero singular values
-m = 5000
-n = 40000
-k = 500
+numpy.random.seed(100)
+m = 2000
+n = 2000
+k = 300
 alpha = 0.05
 print('\n generating the matrix, may take a while...')
 f_sigma = lambda t: 2**(-alpha*t).astype(numpy.float32)
@@ -51,25 +38,16 @@ sigma0, u0, v0, A = random_matrix_for_svd(m, n, k, f_sigma, numpy.float32)
 a = 2*numpy.random.rand(m, n).astype(numpy.float32) - 1
 s = numpy.linalg.norm(a, axis = 0)
 a /= s
-A += 1e-3*a
+A += 5e-3*a
 print('\n computing 144 largest singular values...')
 u, sigma, vt = svds(A, k = 144)
 print('\n 144 largest singular values from svds:')
 print(sigma)
 
-# another test: svds computes rubbish
-numpy.random.seed(1)
-m = 16000
-n = 16000
-k = 20
-alpha = 0.01
-print('\n generating the matrix, may take a while...')
-f_sigma = lambda t: 2**(-alpha*t).astype(numpy.float32)
-sigma0, u0, v0, A = random_matrix_for_svd(m, n, k, f_sigma, numpy.float32)
-a = 2*numpy.random.rand(m, n).astype(numpy.float32) - 1
-s = numpy.linalg.norm(a, axis = 0)
-A += a*(min(sigma0[-1], 1e-3)/s)
-print('\n computing 128 largest singular values...')
-u, sigma, vt = svds(A, k = 128)
-print('\n 128 largest singular values from svds:')
-print(sigma)
+u, sigma0, vt = svd(A)
+print('\n 144 largest singular values from svd:')
+print(sigma0[:144])
+print('\n next 5 largest singular values:')
+print(sigma0[144:149])
+print('\n smallest 5 singular values:')
+print(sigma0[-5:])
