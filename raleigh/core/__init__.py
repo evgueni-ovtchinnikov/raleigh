741fd892d6bbc02a7fa64dbc88453dc4aeed6f7c
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2019-06-12 11:22:01 +0100

    added missing __init__ to module core

---
+++ raleigh/core/__init__.py
@@ -0,0 +1,7 @@
+'''RALEIGH core solver.
+
+    Contains core eigensolver routine implemented in terms of an abstract
+    Vectors class.
+    Direct use is for advanced users only - start by trying user-friendly
+    interfaces in drivers.
+'''
