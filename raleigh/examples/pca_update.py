ec5e5bbb497daa445fae5d1c24db724934b78c7d
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2019-10-09 13:40:53 +0100

    added timing to pca_update.py

--- raleigh/examples/pca_update.py
+++ raleigh/examples/pca_update.py
@@ -18,6 +18,7 @@ max_pcs   : maximal number of principal components to compute (<1: no limit)
 
 import numpy
 import sys
+import timeit
 
 # in case this raleigh package is not pip installed (e.g. cloned from github)
 raleigh_path = '../..'
@@ -41,21 +42,28 @@ m_all = data.shape[0]
 if len(data.shape) > 2: # allow for multi-dimensional samples (e.g. images)
     n = numpy.prod(data.shape[1:])
     data = numpy.reshape(data, (m_all, n))
-m = min(m_all - 1, max(1, int(q*m_all)))
+m = min(m_all, max(1, int(q*m_all)))
 
 print('computing PCs for %d data samples...' % m)
+start = timeit.default_timer()
 mean, trans, comps = pca(data[: m, :], tol=atol, mpc=mpc, verb=1, arch=arch)
-print('%d principal components computed' % trans.shape[1])
+elapsed = timeit.default_timer() - start
+ncomp = comps.shape[0]
+print('%d principal components computed in %.2e sec' % (ncomp, elapsed))
 em, ef = pca_error(data[: m, :], mean, trans, comps)
 print('PCA error: max %.1e, Frobenius %.1e' % (em, ef))
 
-print('\nmore data arrived, updating PCs for %d data samples...' % m_all)
-if atol == 0 and mpc < 1: # interactive mode not helpful in update
-    mpc = comps.shape[0]
-mean, trans, comps = pca(data[m :, :], mpc=mpc, tol=atol, verb=0, arch=arch, \
-    have=(mean, trans, comps))
-print('%d principal components computed' % trans.shape[1])
-em, ef = pca_error(data, mean, trans, comps)
-print('PCA error: max %.1e, Frobenius %.1e' % (em, ef))
+if m < m_all:
+    print('\nmore data arrived, updating PCs for %d data samples...' % m_all)
+    start = timeit.default_timer()
+    if atol == 0 and mpc < 1: # interactive mode not helpful in update
+        mpc = comps.shape[0]
+    mean, trans, comps = pca(data[m :, :], mpc=mpc, tol=atol, verb=0, arch=arch, \
+        have=(mean, trans, comps))
+    elapsed = timeit.default_timer() - start
+    ncomp = comps.shape[0]
+    print('%d principal components updated in %.2e sec' % (ncomp, elapsed))
+    em, ef = pca_error(data, mean, trans, comps)
+    print('PCA error: max %.1e, Frobenius %.1e' % (em, ef))
 
 print('done')
