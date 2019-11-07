cb3763a901a81c23bbcc8c358e55126d8abd809c
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2019-11-07 16:03:44 +0000

    updated __init__.py's

--- raleigh/__init__.py
+++ raleigh/__init__.py
@@ -4,17 +4,17 @@ core:
     Contains core eigensolver routine implemented in terms of an abstract
     Vectors class.
     Direct use is for advanced users only - start by trying user-friendly
-    interfaces in drivers.
+    interfaces in ./interfaces.
 
 algebra:
     Implementations of Vectors class (and related matrix classes) on CPU and
     GPU.
 
-drivers:
+interfaces:
     User-friendly interfaces to solver.
 
 examples:
-    Example scripts demonstrating the use of drivers for the solution of
+    Example scripts demonstrating the use of interfaces for the solution of
     eigenvalue problems for sparse matrices and singular value problems for
     dense matrices.
 '''
--- raleigh/core/__init__.py
+++ raleigh/core/__init__.py
@@ -2,6 +2,7 @@
 
     Contains core eigensolver routine implemented in terms of an abstract
     Vectors class.
+
     Direct use is for advanced users only - start by trying user-friendly
-    interfaces in drivers.
+    interfaces in raleigh/interfaces.
 '''
