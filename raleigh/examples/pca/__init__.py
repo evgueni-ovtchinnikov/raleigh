b98193b0346be39e288c7b9c6ba14917d7bab7a9
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2019-11-27 13:31:13 +0000

    put brief description of pca demos in examples/pca/__init__.py

--- raleigh/examples/pca/__init__.py
+++ raleigh/examples/pca/__init__.py
@@ -1,2 +1,34 @@
-'''Example scripts.
+'''Example and utility scripts.
+
+pca_simple.py:
+    Computes a number of principal components of a dataset.
+    If sklearn is installed, compares with sklearn.decomposition.PCA.
+
+pca_smart.py:
+    Computes PCA approximation of specified accuracy.
+    If sklearn is installed, compares with restarted sklearn.decomposition.PCA.
+
+pca_update.py:
+    Performs PCA on a chunk of data, then addds more data and updates principal 
+    components.
+
+incremental_pca.py:
+    Incremental Principal Component Analysis demo.
+    Principal components are computed incrementally, processing a number 
+    (batch size) of data samples at a time.
+    For comparison, the same amount of principal components is computed also by 
+    scikit-learn IncrementalPCA.
+
+interactive_pca.py:
+    Computes principal components of a dataset until stopped by the user.
+
+generate_matrix.py:
+   Generates a data matrix from randomly generated singular vectors and 
+   singular values of user-controlled behaviour.
+   If run as a script, the generated matrix is saved to data.npy, and singular
+   values and vectors to svd.npz.
+   Alternatively, from generate_matrix import generate, after which the call of
+   generate(m, n, rank, dtype, scale, alpha, pca) will return the matrix,
+   singular values and left and right singular vectors.
+
 '''
