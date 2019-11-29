20adb5da9b1917b48e423f6fb37effb193397d85
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2019-11-29 09:00:07 +0000

    incremented version tag to 1.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="raleigh",
-    version="1.1.3",
+    version="1.1.4",
     author="Evgueni Ovtchinnikov",
     author_email="evgueni.ovtchinnikov@stfc.ac.uk",
     description="RAL eigensolver for real symmetric and Hermitian problems",
