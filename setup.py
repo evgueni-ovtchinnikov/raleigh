baabc815b3788daea4e9e4cbfe63a87f896ad4c7
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2020-04-30 12:45:34 +0100

    incremented raleigh version to 1.3.2 preparing for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="raleigh",
-    version="1.3.1",
+    version="1.3.2",
     author="Evgueni Ovtchinnikov",
     author_email="evgueni.ovtchinnikov@stfc.ac.uk",
     description="RAL eigensolver for real symmetric and Hermitian problems",
