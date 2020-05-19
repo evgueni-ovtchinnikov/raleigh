a5eada5ba2b43ff897cf8d333804a76c111caa3c
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2020-05-19 16:29:11 +0100

    allowed for variable number of outputs from MATLAB interface to partial_hevp

--- raleigh/interfaces/partial_hevp.m
+++ raleigh/interfaces/partial_hevp.m
@@ -1,7 +1,8 @@
-function [lambda, x] = partial_hevp(matrixA, nep, varargin)
+function varargout = partial_hevp(matrixA, nep, varargin)
 
 import py.raleigh.interfaces.matlab.part_hevp
 
+nep = int32(nep);
 rowB = [];
 colB = [];
 valB = [];
@@ -20,5 +21,10 @@ msize = size(matrixA);
 n = msize(1);
 [rowA, colA, valA] = find(matrixA);
 rval = part_hevp(n, rowA, colA, valA, nep, sigma, rowB, colB, valB, opts);
-lambda = double(rval{1});
-x = double(rval{2});
+for i = 1 : nargout
+    if i < 3
+        varargout{i} = double(rval{i});
+    else
+        varargout{i} = int64(rval{i});
+    end
+end
